81745b2f84759b6a9c9fcd2f8329281770992b74
DootMan27 <tvdootman27@gmail.com>
2025-05-28 22:36:29 +0200

    now using PersistentProjectileEntity

--- src/main/java/nl/sniffiandros/bren/common/entity/BulletEntity.java
+++ src/main/java/nl/sniffiandros/bren/common/entity/BulletEntity.java
@@ -1,12 +1,8 @@
 package nl.sniffiandros.bren.common.entity;
 
-import com.mojang.datafixers.types.templates.Tag;
-import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.fabricmc.fabric.api.tag.convention.v1.ConventionalBlockTags;
-import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
-import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.block.entity.EndGatewayBlockEntity;
+import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
@@ -14,39 +10,37 @@ import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.data.DataTracker;
 import net.minecraft.entity.data.TrackedData;
 import net.minecraft.entity.data.TrackedDataHandlerRegistry;
-import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.mob.EndermanEntity;
-import net.minecraft.entity.passive.ParrotEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.projectile.ProjectileEntity;
-import net.minecraft.entity.projectile.ProjectileUtil;
+import net.minecraft.entity.projectile.PersistentProjectileEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.particle.BlockStateParticleEffect;
 import net.minecraft.particle.ParticleTypes;
-import net.minecraft.potion.Potion;
-import net.minecraft.potion.PotionUtil;
-import net.minecraft.registry.tag.BlockTags;
-import net.minecraft.registry.tag.TagKey;
 import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.RaycastContext;
 import net.minecraft.world.World;
-import nl.sniffiandros.bren.common.Bren;
+import net.minecraft.entity.projectile.ProjectileUtil;
 import nl.sniffiandros.bren.common.config.MConfig;
 import nl.sniffiandros.bren.common.registry.DamageTypeReg;
 import nl.sniffiandros.bren.common.registry.ParticleReg;
+import nl.sniffiandros.bren.common.Bren;
 
-import java.util.Iterator;
-
-public class BulletEntity extends ProjectileEntity {
+public class BulletEntity extends PersistentProjectileEntity {
     private static final TrackedData<Integer> LIFESPAN = DataTracker.registerData(BulletEntity.class, TrackedDataHandlerRegistry.INTEGER);
     private float damage;
 
     public BulletEntity(EntityType<? extends BulletEntity> entityType, World world) {
         super(entityType, world);
+        this.setNoGravity(true);
+        this.pickupType = PickupPermission.DISALLOWED;
     }
 
     public BulletEntity(World world, float damage, int lifespan, LivingEntity owner) {
@@ -55,6 +49,7 @@ public class BulletEntity extends ProjectileEntity {
         this.setLifespan(lifespan);
         this.setNoGravity(true);
         this.setOwner(owner);
+        this.pickupType = PickupPermission.DISALLOWED;
     }
 
     protected void setLifespan(int lifespan) {
@@ -67,113 +62,103 @@ public class BulletEntity extends ProjectileEntity {
 
     @Override
     protected void initDataTracker() {
+        super.initDataTracker();
         this.dataTracker.startTracking(LIFESPAN, 0);
     }
 
+    @Override
     public void tick() {
-        float h;
+        // Store current (pre-tick) position
+        Vec3d oldPos = this.getPos();
+
+        // Apply vanilla movement (updates position, velocity, etc.)
         super.tick();
-        HitResult hitResult = ProjectileUtil.getCollision(this, this::canHit);
-        boolean bl = false;
-        if (hitResult.getType() == HitResult.Type.BLOCK) {
-            BlockPos blockPos = ((BlockHitResult)hitResult).getBlockPos();
-            BlockState blockState = this.getWorld().getBlockState(blockPos);
-            if (blockState.isOf(Blocks.NETHER_PORTAL)) {
-                this.setInNetherPortal(blockPos);
-                bl = true;
-            } else if (blockState.isOf(Blocks.END_GATEWAY)) {
-                BlockEntity blockEntity = this.getWorld().getBlockEntity(blockPos);
-                if (blockEntity instanceof EndGatewayBlockEntity && EndGatewayBlockEntity.canTeleport(this)) {
-                    EndGatewayBlockEntity.tryTeleportingEntity(this.getWorld(), blockPos, blockState, this, (EndGatewayBlockEntity)blockEntity);
-                }
-                bl = true;
+
+        // Compute new position after movement
+        Vec3d newPos = this.getPos();
+
+        // Raytrace for blocks
+        BlockHitResult blockHit = this.getWorld().raycast(new RaycastContext(
+                oldPos, newPos,
+                RaycastContext.ShapeType.COLLIDER,
+                RaycastContext.FluidHandling.NONE,
+                this
+        ));
+
+        // Set up for minimum distance check
+        double minDist = Double.MAX_VALUE;
+        HitResult hitResult = null;
+
+        // If block hit, store it and its distance
+        if (blockHit.getType() != HitResult.Type.MISS) {
+            minDist = oldPos.squaredDistanceTo(blockHit.getPos());
+            hitResult = blockHit;
+        }
+
+        // Raytrace for entities
+        Box boundingBox = this.getBoundingBox().stretch(this.getVelocity()).expand(1.0D);
+        EntityHitResult entityHit = ProjectileUtil.getEntityCollision(
+                this.getWorld(),
+                this,
+                oldPos,
+                newPos,
+                boundingBox,
+                (entity) -> entity != this.getOwner() && super.canHit(entity)
+        );
+        if (entityHit != null) {
+            double entityDist = oldPos.squaredDistanceTo(entityHit.getPos());
+            if (entityDist < minDist) {
+                minDist = entityDist;
+                hitResult = entityHit;
             }
         }
-        if (hitResult.getType() != HitResult.Type.MISS && !bl) {
-            this.onCollision(hitResult);
-        }
-        this.checkBlockCollision();
-        Vec3d vec3d = this.getVelocity();
-        double d = this.getX() + vec3d.x;
-        double e = this.getY() + vec3d.y;
-        double f = this.getZ() + vec3d.z;
-        this.updateRotation();
-        if (this.isTouchingWater()) {
-            for (int i = 0; i < 4; ++i) {
-                float g = 0.25f;
-                this.getWorld().addParticle(ParticleTypes.BUBBLE, d - vec3d.x * g, e - vec3d.y * g, f - vec3d.z * g, vec3d.x, vec3d.y, vec3d.z);
+
+        // Handle collision if one occurred
+        if (hitResult != null) {
+            // Set bullet to the hit position
+            Vec3d hitPos = hitResult instanceof EntityHitResult
+                    ? ((EntityHitResult) hitResult).getPos()
+                    : ((BlockHitResult) hitResult).getPos();
+            this.setPos(hitPos.x, hitPos.y, hitPos.z);
+
+            // Call the appropriate handler and return
+            if (hitResult instanceof EntityHitResult) {
+                this.onEntityHit((EntityHitResult) hitResult);
+            } else if (hitResult instanceof BlockHitResult) {
+                this.onBlockHit((BlockHitResult) hitResult);
             }
-            h = 0.8f;
-        } else {
-            h = 0.99f;
-        }
-        this.setVelocity(vec3d.multiply(h));
-        if (!this.hasNoGravity()) {
-            Vec3d vec3d2 = this.getVelocity();
-            this.setVelocity(vec3d2.x, vec3d2.y - (double)this.getGravity(), vec3d2.z);
-        }
-        this.setPosition(d, e, f);
-
-        double l = this.getVelocity().length();
-
-        if (Math.ceil(l) == 0) {
-            this.discard();
+            return; // Stop further movement/processing this tick
         }
 
+        // Air particle effect
+        if (this.getWorld().isClient() && this.age >= 2 && this.age % 3 == 0) {
+            this.getWorld().addParticle(
+                    ParticleReg.AIR_RING_PARTICLE,
+                    this.getX(), this.getY() + this.getHeight() / 2, this.getZ(),
+                    0, 0, 0
+            );
+        }
+
+        // Lifespan check
         if (this.age >= this.getLifespan()) {
             this.discard();
         }
-
-        if (this.getWorld().isClient() && this.age >= 2 && this.age % 3 == 0) {
-            this.getWorld().addParticle(ParticleReg.AIR_RING_PARTICLE, this.getX(), this.getY() + this.getHeight()/2, this.getZ(), 0, 0, 0);
-        }
     }
 
-    protected float getGravity() {
-        return 0.03f;
+    @Override
+    protected boolean canHit(Entity entity) {
+        // Never hit the shooter, but allow hitting anything else immediately
+        return entity != this.getOwner() && super.canHit(entity);
     }
 
+    @Override
     protected void onEntityHit(EntityHitResult entityHitResult) {
-        super.onEntityHit(entityHitResult);
         Entity entity = entityHitResult.getEntity();
+        if (entity.equals(this.getOwner())) return;
 
-        // This is to make Enderman teleport away instead of letting themselves be hit by bullets, this is to simulate their behavior when a projectile such as arrows or snowballs tries to hit them
-        if (entity instanceof EndermanEntity enderman) {
-            for (int i = 0; i < 100; i++) {
-                double randomX = enderman.getX() + (Math.random() - 0.5) * 16;
-                double randomY = enderman.getY() + (Math.random() - 0.5) * 8;
-                double randomZ = enderman.getZ() + (Math.random() - 0.5) * 16;
-
-                BlockPos targetPos = new BlockPos((int) randomX, (int) randomY, (int) randomZ);
-                BlockState blockState = enderman.getWorld().getBlockState(targetPos);
-
-                if (blockState.isAir() && enderman.getWorld().getBlockState(targetPos.down()).isSolidBlock(enderman.getWorld(), targetPos.down())) {
-                    enderman.teleport(randomX, randomY + 1, randomZ);
-                    break;
-                }
-            }
-            return; // Prevent damage towards Enderman
-        }
-
-        // This is to fix bullets not being able to destroy End Crystals
-        if (entity.getType() == EntityType.END_CRYSTAL) {
-            DamageSource damageSource = DamageTypeReg.shot(this.getWorld(), this, this.getOwner());
-            entity.damage(damageSource, 1f); // Makes End Crystals explode when hit
-        }
-
-        // Fixes Ender Dragon not taking damage from bullets
-        if (entity.getType() == EntityType.ENDER_DRAGON) {
-            DamageSource damageSource = DamageTypeReg.shot(this.getWorld(), this, this.getOwner());
-
-            entity.damage(damageSource, this.damage);
-            entity.damage(damageSource, this.damage * 0.5f); // Apply an additional damage instance because "livingEntity.timeUntilRegen = 0;" isn't compatible with "EntityType.ENDER_DRAGON"
-        }
-
-        if (entity.equals(this.getOwner())) {
-            return;
-        }
-
-        if (entity instanceof LivingEntity livingEntity) {
+        if (entity instanceof EndermanEntity) {
+            super.onEntityHit(entityHitResult);
+        } else if (entity instanceof LivingEntity livingEntity) {
             livingEntity.timeUntilRegen = 0;
             DamageSource damageSource = DamageTypeReg.shot(this.getWorld(), this, this.getOwner());
             livingEntity.damage(damageSource, this.damage);
@@ -185,41 +170,47 @@ public class BulletEntity extends ProjectileEntity {
         this.discard();
     }
 
-
     @Override
     protected void onBlockHit(BlockHitResult blockHitResult) {
         super.onBlockHit(blockHitResult);
+
         BlockPos pos = blockHitResult.getBlockPos();
         BlockState state = this.getWorld().getBlockState(pos);
-
         Vec3d vec3d = blockHitResult.getPos();
 
-        if (!state.isAir() && state.isSolid() && this.age > 1) {
-
+        if (!state.isAir() && state.isSolid()) {
             if ((state.isIn(ConventionalBlockTags.GLASS_BLOCKS) || state.isIn(ConventionalBlockTags.GLASS_PANES)) && MConfig.bulletsBreakGlass.get()) {
-                if (this.getWorld().isClient()) { return;}
-                this.getWorld().breakBlock(pos, false, this.getOwner());
+                if (!this.getWorld().isClient()) {
+                    this.getWorld().breakBlock(pos, false, this.getOwner());
+                }
             } else {
-                this.getWorld().playSound(null,vec3d.x,vec3d.y,vec3d.z,state.getSoundGroup().getBreakSound(), SoundCategory.BLOCKS, 1.0F, 3.0F);
+                this.getWorld().playSound(null, vec3d.x, vec3d.y, vec3d.z, state.getSoundGroup().getBreakSound(), SoundCategory.BLOCKS, 1.0F, 3.0F);
 
                 boolean isAirAbove = this.getWorld().getBlockState(pos.up()).isAir();
                 boolean hitGround = !this.getWorld().getBlockState(pos).isAir();
 
                 if (this.getWorld().isClient()) {
                     for (int i = 0; i < 4; ++i) {
-
                         float x = this.random.nextFloat() - .5f;
                         float y = this.random.nextFloat() - .5f;
                         float z = this.random.nextFloat() - .5f;
-
-                        this.getWorld().addParticle(new BlockStateParticleEffect(ParticleTypes.BLOCK, state), vec3d.x,vec3d.y,vec3d.z, x, y, z);
+                        this.getWorld().addParticle(new BlockStateParticleEffect(ParticleTypes.BLOCK, state), vec3d.x, vec3d.y, vec3d.z, x, y, z);
                     }
-                } else if (hitGround && isAirAbove && this.isOnFire()){
+                } else if (hitGround && isAirAbove && this.isOnFire()) {
                     this.getWorld().setBlockState(pos.up(), Blocks.FIRE.getDefaultState());
                 }
-
-                this.discard();
             }
+            this.discard();
         }
+    }
+
+    @Override
+    protected ItemStack asItemStack() {
+        return ItemStack.EMPTY;
+    }
+
+    @Override
+    protected SoundEvent getHitSound() {
+        return SoundEvents.BLOCK_WOOL_HIT;
     }
 }
